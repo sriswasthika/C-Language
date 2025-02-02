05103ba0c6248e21d3fd827b219e0136212f3e2f
sriswasthika <155260452+sriswasthika@users.noreply.github.com>
2025-02-02 15:12:25 +0530

    Create My-1st-code.c

---
+++ Input and Output/My-1st-code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   
+	printf("\n*************VIWISE TECH*************");
+	printf("\n\"Welcome to The World of Programming\"");
+	printf("\n\t@@@@Calm with Coding@@@@");
+	printf("\n\t\tThank you!");
+	printf("\n*************************************");
+
+    return 0;
+}
